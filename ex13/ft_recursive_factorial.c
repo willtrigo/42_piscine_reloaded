7fdc31301b595137ad35ccf4e67472be9e11d5ed
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-07-19 18:06:34 -0300

    Add ex13/ft_recursive_factorial, this function receive one arg and return your factorial result, but this function is implemented with recursive solution.  Changes to be committed: 	new file:   ft_recursive_factorial.c

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/18 18:27:49 by dande-je          #+#    #+#             */
+/*   Updated: 2023/07/18 22:45:17 by dande-je         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 0)
+		return (1);
+	return (nb * ft_recursive_factorial(nb - 1));
+}
