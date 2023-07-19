b0cb27e60de862e1221dece9d67aa74a9255c42b
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-07-19 19:00:02 -0300

    Trying another way to resolve this problem, now this function has tail recursive to resolve the factorial problem.  Changes to be committed: 	modified:   ex13/ft_recursive_factorial.c

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,15 +6,20 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 18:27:49 by dande-je          #+#    #+#             */
-/*   Updated: 2023/07/18 22:45:17 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/07/19 18:54:02 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int	ft_recursive_factorial_aux(int nb, int factorial)
+{
+	if (nb <= 1)
+		return (factorial);
+	return (ft_recursive_factorial_aux(nb - 1, nb * factorial));
+}
 
 int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0)
 		return (0);
-	else if (nb == 0)
-		return (1);
-	return (nb * ft_recursive_factorial(nb - 1));
+	return (ft_recursive_factorial_aux(nb, 1));
 }
